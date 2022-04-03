4d8608b3334809e81190ea94964f00f1bd7f05b9
anjwoc <anjwoc@naver.com>
2022-04-04 03:04:53 +0900

    계정 변경 테스트

--- src/main/java/com/example/restfulwebservice/user/AdminUserController.java
+++ src/main/java/com/example/restfulwebservice/user/AdminUserController.java
@@ -24,6 +24,7 @@ public class AdminUserController {
         this.service = service;
     }
 
+    // test
     @GetMapping("/users")
     public MappingJacksonValue retrieveAllUsers() {
         List<User> users = service.findAll();
